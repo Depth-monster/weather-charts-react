8f9173e6cbb9ec990e4670078656ff92ac3c3103
Azat <azat1hajy@gmail.com>
2023-02-09 03:44:41 +0300

    API key and axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,42 @@
 import Search from './components/Search/Search'
+import WeatherContainer from './components/WeatherContainer/WeatherContainer';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 
 function App() {
+  const API: string = '814a79c17d4df4e9e8b2bcacf2722dc8';
+  let cityId: number = 5128581; // New York City ID
+  let [weatherData, setWeatherData] = useState<any>(null);
+
+  useEffect(() => {
+    axios
+    
+      .get(`https://api.openweathermap.org/data/2.5/weather?q=texas&appid=814a79c17d4df4e9e8b2bcacf2722dc8`)
+      //.get(`https://api.openweathermap.org/data/2.5/weather?id=${cityId}&appid=${API}`)
+      //.get(`https://api.openweathermap.org/data/2.5/weather?q=814a79c17d4df4e9e8b2bcacf2722dc8`)
+      .then(response => {
+        setWeatherData(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
   return (
-    //App: background pink
     <div className="App">
       {/* AppGlass background milk */}
       <div className="AppGlass">
         {/* just container*/}
         <div className="container">
           <h1>Weather</h1>
-
           <Search />
-          
 
+          {weatherData ? (
+            <WeatherContainer weatherData={weatherData} />
+          ) : (
+            <p>Loading...</p>
+          )}
         </div>
       </div>
     </div>
--- src/components/WeatherContainer/WeatherContainer.tsx
+++ src/components/WeatherContainer/WeatherContainer.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
 
-const WeatherContainer = () => {
-    return (
+const WeatherContainer = ({ weatherData}:any) => {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'spaceBetween' }}>
+      <div>
         <div>
-            
+          <span>Среднесуточная t°</span>
+          <div>
+            {weatherData.main.temp}
+            <span>°C</span>
+          </div>
         </div>
-    );
+        <div>
+          <ul>
+            <li>Probability: {weatherData.clouds.all}%</li>
+            <li>Humidity: {weatherData.main.humidity}%</li>
+            <li>Windspeed: {weatherData.wind.speed} m/s</li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        {weatherData.name}
+        <div>Population: {weatherData.population}</div>
+        <div>{new Date(weatherData.dt * 1000).toLocaleDateString()}</div>
+        <div>{weatherData.weather[0].description}</div>
+      </div>
+    </div>
+  );
 };
 
 export default WeatherContainer;
